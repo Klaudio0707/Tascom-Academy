b4f97e2491fb5115c0bd75899e6bba84d2ec199c
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-03-15 16:25:02 -0300

    introducao de logica de programacao utilizando TS

---
+++ Logica/logica.ts
@@ -0,0 +1,20 @@
+// const idade: number = 28;
+
+// const nome: string = "Claudio";
+
+// const ativo: boolean = true;
+
+// const numeros: number[] = [1,2,3];
+
+// const pessoa: {nome: string, idade: number} ={
+//     nome: "Claudio",
+//     idade: 28
+// };
+
+// enum diaDaSemana{
+//     segunda,
+//     terca,
+//     quarta,
+//     quinta,
+//     sexta
+// };
