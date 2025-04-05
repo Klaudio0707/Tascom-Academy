feb6c157e1d2478ee1091ef0314ec291bbe6c9e2
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-04-05 20:42:05 -0300

    ATV: converso de temperatura

--- ambiente_teste/src/index.ts
+++ ambiente_teste/src/index.ts
@@ -21,10 +21,102 @@
 
 // console.log(resultado);
 
-let valueEntrada: number = 6;
+// let valueEntrada: number = 6;
 
-if (valueEntrada % 2 == 0) {
-  console.log(`Valor ${valueEntrada} é par`);
-} else {
-  console.log(`Valor ${valueEntrada}  é impar`); 
-}
+// if (valueEntrada % 2 == 0) {
+//   console.log(`Valor ${valueEntrada} é par`);
+// } else {
+//   console.log(`Valor ${valueEntrada}  é impar`);
+// }
+
+// let idade: number = 15;
+
+// if (idade >= 18 && idade <= 24) {
+//   console.log("Adulto jovem");
+// } else if (idade >= 25) {
+//   console.log("adulto");
+// } else if (idade <= 5) {
+//   console.log("bebe");
+// } else if (idade <= 12) {
+//   console.log("criança");
+// } else {
+//   console.log("adolescente");
+// }
+
+
+// const ano: number = 2000;
+
+// if(ano % 4 == 0 && ano % 400 == 0 || ano % 100 !== 0 ){
+//  console.log("bissexto")
+
+// }else{
+//     console.log("não bissexto");
+
+// }
+
+// const valorInput1: number = 10;
+// const valorInput2: number = 2;
+// let operacao: string = "-jhghk";
+ 
+//  switch (operacao) {
+// case "-" : console.log(valorInput1 - valorInput2); 
+// break;
+ 
+//  case "+" : console.log(valorInput1 + valorInput2); 
+// break;
+
+// case "/" : console.log(valorInput1 / valorInput2); 
+// break;
+
+// case "*" : console.log(valorInput1 * valorInput2); 
+// break;
+// default:
+//     console.log("Operador inválido")
+// }
+ 
+
+// let numb: number = 100;
+
+
+//     for (let i:number = 0; i <= numb; i++) {
+        
+//         if( i % 2 === 0){
+//             console.log(`${i} par`)
+//         }
+//     }
+
+
+// let multiplicador: number = 3;
+// let resultado: number = 0;
+
+// if(multiplicador >= 0){
+
+//     for (let valor= 1; valor <= 10; valor++) {
+//         resultado = multiplicador * valor;
+//         console.log(`${valor} x ${multiplicador}=`, resultado)
+        
+        
+//     }
+// }else{
+//     console.log("informe um valor positivo")
+// }
+
+
+// let numero: number = 148;
+// let div: number = 10;
+// let incrementador: number = 0;
+// while(numero !== 0){
+//     numero = Math.floor(numero/ div);
+//     incrementador++;
+
+// }
+//     console.log(incrementador)
+    
+
+
+
+import readlineSync from "readline-sync";
+
+// let pergunta = readlineSync.question("Qual seu nome")
+
+    
