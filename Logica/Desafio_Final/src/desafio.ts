a02d5cc826668481721634ca0610ed67e533a3f3
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-04-20 19:32:58 -0300

    criação do enums candidatos para testar algumas possibilidades

--- Logica/Desafio_Final/src/desafio.ts
+++ Logica/Desafio_Final/src/desafio.ts
@@ -1,2 +1,21 @@
-console.clear();
-console.log("Hello word")
+//imports
+import * as readline from 'readline-sync';
+
+
+enum Candidatos {
+    Prefeitura = 1,
+    CamaraVereadores = 2,
+}
+const opcao: number = readline.questionInt("Digite um numero de 1 a 2 ")
+
+const checkStatus = (status:number) =>{
+switch(status) {
+    case Candidatos.Prefeitura:
+        console.log("Prefeito")
+    break;
+    case Candidatos.CamaraVereadores:
+        console.log("Vereador");
+        break;
+}
+}
+checkStatus(opcao);
