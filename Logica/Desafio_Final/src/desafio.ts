db867f1f7aaa07b2342df8e80fdab1c2c11f2296
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-04-20 22:47:16 -0300

    criei as interfaces para tipa os arrays de objeto candidatos e eleitores

--- Logica/Desafio_Final/src/desafio.ts
+++ Logica/Desafio_Final/src/desafio.ts
@@ -1,21 +1,79 @@
 //imports
 import * as readline from 'readline-sync';
-
-
-enum Candidatos {
+interface Candidato {
+    nome: string,
+    rg: number,
+    chapa: string,
+    numero: number,
+    tipo: string
+}
+interface Eleitor {
+    nome: string,
+    rg: number,
+    apto: boolean,
+    dataVoto: Date | null,
+}
+//enums
+enum Cargos {
     Prefeitura = 1,
     CamaraVereadores = 2,
 }
-const opcao: number = readline.questionInt("Digite um numero de 1 a 2 ")
+let Candidados: Candidato[] = [
+    { nome: "moaci", rg: 103040, chapa: "", numero: 13, tipo: "Prefeitura" }
+]
 
-const checkStatus = (status:number) =>{
-switch(status) {
-    case Candidatos.Prefeitura:
-        console.log("Prefeito")
-    break;
-    case Candidatos.CamaraVereadores:
-        console.log("Vereador");
-        break;
+
+let Eleitores: Eleitor[] = [
+    { nome: "Pablo", rg: 103040, apto: true, dataVoto: new Date("24-10-06") }
+]
+
+//variaveis de escopo globais
+let status: boolean | string = false;
+let opcoes: number = 0;
+
+
+
+do {
+    console.log("Seja bem vindo a votação Municipal da cidade de Quixabinha ");
+
+    status = readline.keyInYN("Deseja iniciar utilizar o sistema de votacao? ");
+
+    if (status === true) {
+        opcoes = readline.questionFloat("Cadastrar um Candidado = Digite 1 n\Cadastrar Eleitor = Digite 2 \nRealizar o voto = Digite 3 ");
+        switch (opcoes) {
+            case 1:
+                console.log("Cadastro de Candidato")
+                status = readline.keyInYN("Deseja continuar ?")
+                break;
+
+            case 2:
+                console.log("Cadastro de Eleitor")
+                break;
+
+            case 3:
+                console.log("Efetuar Voto")
+                break;
+            case 4:
+
+            default:
+                break;
+        }
+    }
+    console.log(status);
+
+    //     const opcao: number = readline.questionInt("Digite um numero de 1 a 2 ")
+
+    //     const checkStatus = (status:number) =>{
+    //         switch(status) {
+    //     case Cargos.Prefeitura:
+    //         console.log("Prefeito")
+    //     break;
+    //     case Cargos.CamaraVereadores:
+    //         console.log("Vereador");
+    //         break;
+    // }
+    // }
+    // checkStatus(opcao);
+} while (status) {
+    console.log("Obrigado e volte sempre");
 }
-}
-checkStatus(opcao);
