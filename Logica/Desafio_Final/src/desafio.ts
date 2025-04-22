23a9655b95de100d0433bbd2cf7b149a92fbb7f4
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-04-21 22:35:36 -0300

    validação de cpf por candidato feita e criação de função para os inputs do console

--- Logica/Desafio_Final/src/desafio.ts
+++ Logica/Desafio_Final/src/desafio.ts
@@ -3,14 +3,14 @@ import * as readline from 'readline-sync';
 //interface para tipagem dos dois arrays de objetos
 interface Candidato {
     nome: string,
-    rg: number,
+    cpf: number,
     chapa: string,
     numero: number,
     candidatura: string
 }
 interface Eleitor {
     nome: string,
-    rg: number,
+    cpf: number,
     apto: boolean,
     dataVoto: Date | null,
 }
@@ -20,12 +20,12 @@ enum Cargos {
     Vereador = 2,
 }
 let Candidatos: Candidato[] = [
-    { nome: "moaci", rg: 103040, chapa: "", numero: 10, candidatura: "Prefeitura" }
+    { nome: "moaci", cpf: 103040, chapa: "trabalhadores", numero: 10, candidatura: "Prefeitura" }
 ]
 
 
 let Eleitores: Eleitor[] = [
-    { nome: "Pablo", rg: 103040, apto: true, dataVoto: new Date("24-10-06") }
+    { nome: "Pablo", cpf: 103040, apto: true, dataVoto: new Date("24-10-06") }
 ]
 
 //variaveis de escopo globais
@@ -33,27 +33,62 @@ let status: boolean | string = false;
 let opcoes: number = 0;
 
 let nome: string = "";
-let rg: number = 0;
+let cpf: number = 0;
 let chapa: string = "";
 let numero: number = 0;
 let tipoCandidato: number = 0;
+let apto: boolean = false;
 
+const inputsCandidato = (): Candidato => {
+    const nome = readline.question("Digite o nome do candidato: ");
+    const cpf = readline.questionInt("Informe o CPF: ");
 
-   const inputsConsole =(): void => {
-            
-            nome= readline.question("Digite o nome do candidato ");
-            rg = readline.questionInt("Informe o rg ");
-            chapa = readline.question("Digite o nome da chapa do candidato ");
-            numero  = readline.questionInt("Digite o numero ");
-            tipoCandidato = readline.questionInt("Tipo 1 para Prefeito(a) e 2 para Vereador(a) ")
-
+    const jaExiste = Candidatos.some(c => c.cpf === cpf);
+    if (jaExiste) {
+        console.log("❌ Candidato já cadastrado!");
+        return null as any; // você pode tratar isso melhor depois
     }
-    const cadastrarCandidato = (nomeCandidato: string, rgCandidato: number,chapaCandidato:string, numeroCandidato: number, tipoCandidato: string) => {
-        const novoCandidato: Candidato = ({ nome: nomeCandidato, rg: rgCandidato, chapa: chapaCandidato, numero: numeroCandidato, candidatura: tipoCandidato })
-        Candidatos.push(novoCandidato);
-        return novoCandidato;
-    }
-    status = readline.keyInYN("Deseja iniciar utilizar o sistema de votacao? ");
+
+    const chapa = readline.question("Digite o nome da chapa do candidato: ");
+    const numero = readline.questionInt("Digite o número: ");
+    const tipoCandidato = readline.questionInt("Tipo 1 para Prefeito(a) e 2 para Vereador(a): ");
+
+    const candidatura = Cargos[tipoCandidato];
+
+    return { nome, cpf, chapa, numero, candidatura };
+};
+
+const candidatoExiste = (cpf: number): boolean => {
+    return Candidatos.filter(candidato => candidato.cpf === cpf)[0] !== undefined;
+};
+
+
+// const filterCandidatos = (inputCpf: number,) => {
+//     const filtro = Candidatos.filter(candidato => {
+//         const aptoCandidato: boolean = candidato.cpf === inputCpf
+//         return aptoCandidato;
+
+//     });
+//     console.log(filtro, "Candidato já cadastrado");
+//     return filtro;
+// }
+
+const inputsEleitor = (): void => {
+    nome = readline.question("Digite o nome do eleitor ");
+    cpf = readline.questionInt("Informe o seu RG ");
+
+    console.table(Candidatos);
+    numero = readline.questionInt("Digite o numero do candidato para realizar seu voto");
+}
+
+
+
+// const cadastrarCandidato = (nomeCandidato: string, rgCandidato: number, chapaCandidato: string, numeroCandidato: number, tipoCandidato: string) => {
+//     const novoCandidato: Candidato = ({ nome: nomeCandidato, cpf: rgCandidato, chapa: chapaCandidato, numero: numeroCandidato, candidatura: tipoCandidato })
+//     Candidatos.push(novoCandidato);
+//     return novoCandidato;
+// }
+status = readline.keyInYN("Deseja iniciar utilizar o sistema de votacao? ");
 do {
     console.log("Seja bem vindo a votação Municipal da cidade de Quixabinha ");
 
@@ -62,18 +97,28 @@ do {
         opcoes = readline.questionInt("Cadastrar Candidado = Digite 1 \nCadastrar Eleitor = Digite 2 \nRealizar o voto = Digite 3\n ");
         switch (opcoes) {
             case 1:
-                console.log("Cadastro de Candidato")
-                inputsConsole();
-                cadastrarCandidato(nome, rg, chapa, numero ,Cargos[tipoCandidato]);
-                console.table(Candidatos);
-                status = readline.keyInYN("Deseja continuar ?")
-                break;
+                console.clear();
+    console.log("Cadastro de Candidato");
+    
+    const novoCandidato = inputsCandidato();
+    
+    if (novoCandidato) {
+        Candidatos.push(novoCandidato);
+        console.table(Candidatos);
+    }
+
+    status = readline.keyInYN("Deseja continuar?");
+    break;
 
             case 2:
-                console.log("Cadastro de Eleitor")
+                console.clear();
+                inputsEleitor();
+                // cadastrarCandidato(nome, cpf, chapa, numero, Cargos[tipoCandidato]);
+                // console.log("Cadastro de Eleitor")
                 break;
 
             case 3:
+                console.clear();
                 console.log("Efetuar Voto")
                 break;
             case 4:
@@ -86,19 +131,5 @@ do {
     }
     console.log(status);
 
-    //     const opcao: number = readline.questionInt("Digite um numero de 1 a 2 ")
-
-    //     const checkStatus = (status:number) =>{
-    //         switch(status) {
-    //     case Cargos.Prefeitura:
-    //         inputsConsole();
-    //             cadastrarCandidato(nome, rg, numero);
-    //     break;
-    //     case Cargos.CamaraVereadores:
-    //         console.log("Vereador");
-    //         break;
-    // }
-    // }
-    // checkStatus(opcao);
-} while (status) 
-    console.log("Obrigado e volte sempre");
+} while (status)
+console.log("Obrigado e volte sempre");
