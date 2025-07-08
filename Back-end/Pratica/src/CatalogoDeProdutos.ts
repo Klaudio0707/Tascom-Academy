f0f5d6a1c04a917fae81090c80bb15ab46267068
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-07-07 21:57:10 -0300

    desafio catalogo de produtos

--- Back-end/Pratica/src/CatalogoDeProdutos.ts
+++ Back-end/Pratica/src/CatalogoDeProdutos.ts
@@ -11,10 +11,27 @@ class Produto {
             // readonly significa que não pode ser alterada 
 
     }
-    getId():number{
+    public getId():number{
         return this.id;
     }
-    getNome(): string {
+    public getNome(): string {
         return this.nome;
     }
+     public getPreco():number{
+        return this.preco;
+    }
+    public getQuantidade():number{
+        return this.quantidade
+    }
+    public getPrecoComDesconto(percentual: number){
+        console.log("preço atual: ",this.getPreco())
+        return (
+            (this.preco * percentual)/100
+        )
+    }
 }
+const produtoNovo = new Produto(1222,"caixa",10.5,10);
+console.log("ID produto: ",produtoNovo.getId())
+console.log("Nome produto: ",produtoNovo.getNome())
+console.log("preço produto: ",produtoNovo.getPreco())
+console.log("preço com desconto produto: ",produtoNovo.getPrecoComDesconto(10))
