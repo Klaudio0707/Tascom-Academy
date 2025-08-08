a270aa719181da1dd73b63852533e171cefc656c
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-08-07 22:40:34 -0300

    alteração do public para declare para uso do sequelize

--- Back-end/Nestjs/aula19072025/api-teste/src/modules/vehicle/vehicle.entity.ts
+++ Back-end/Nestjs/aula19072025/api-teste/src/modules/vehicle/vehicle.entity.ts
@@ -4,54 +4,54 @@ import { CarModel } from "../carModel/carModel.entity";
 
 
 @Table({
-        tableName: "vehicle",
-        timestamps:true
-    })
+    tableName: "vehicle",
+    timestamps: true
+})
 export class Vehicle extends Model<Vehicle> {
-        @PrimaryKey
-        @Default(DataType.UUIDV4)
-        @Column(DataType.UUID)
-        public vehicle_id: string
-    
-        @Column({
-            type: DataType.STRING,
-            allowNull: false
-        })
-        placa: string
-        
-        @Column({
-            type: DataType.STRING,
-            allowNull: false
-        })
-        cor: string
-        
-        @Column({
-            type: DataType.INTEGER,
-            allowNull: false
-        })
-        ano_fabricacao: number
-        
-        @Column({
-            type: DataType.INTEGER,
-            allowNull: false
-        })
-        quilometragem: number
-        
-        @ForeignKey(() => User)
-        @Column({
-            type:DataType.UUID,
-            allowNull: false
-        })
-       user_id: string
-       @BelongsTo(() => User)
-       user: User
+    @PrimaryKey
+    @Default(DataType.UUIDV4)
+    @Column(DataType.UUID)
+    declare vehicle_id: string
 
-       @ForeignKey(() => CarModel)
-       @Column({
-           type:DataType.UUID,
-           allowNull: false
-       })
-       model_id: string
-      @BelongsTo(() => CarModel)
-      carMode: CarModel
-    }
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    declare placa: string
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    declare cor: string
+
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false
+    })
+    declare ano_fabricacao: number
+
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false
+    })
+    declare quilometragem: number
+
+    @ForeignKey(() => User)
+    @Column({
+        type: DataType.UUID,
+        allowNull: false
+    })
+    declare user_id: string
+    @BelongsTo(() => User)
+    declare user: User
+
+    @ForeignKey(() => CarModel)
+    @Column({
+        type: DataType.UUID,
+        allowNull: false
+    })
+    declare model_id: string
+    @BelongsTo(() => CarModel)
+    declare carMode: CarModel
+}
