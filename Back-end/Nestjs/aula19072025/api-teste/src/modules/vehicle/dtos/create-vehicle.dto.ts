6a6b01c56a4a5d07cd115f63c2e59f150e07c265
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-08-07 22:34:32 -0300

    update-vehicle.dto -

--- Back-end/Nestjs/aula19072025/api-teste/src/modules/vehicle/dtos/create-vehicle.dto.ts
+++ Back-end/Nestjs/aula19072025/api-teste/src/modules/vehicle/dtos/create-vehicle.dto.ts
@@ -1,13 +1,19 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 
 export class CreateVehicleDto{
+    @IsString()
     @IsNotEmpty()
     placa: string;
+
+    @IsString()
     cor: string;
+
+    @IsNumber()
     ano_fabricacao: number;
+
+    @IsNumber()
     quilometragem: number;
     
-   
 
 }
